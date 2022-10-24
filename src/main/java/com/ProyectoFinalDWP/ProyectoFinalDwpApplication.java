8c44b712ec77793f7c6cd31738e129384f660419
Zujeily <zujeily@gmail.com>
2022-10-23 19:31:52 -0600

    avance practica1

---
+++ src/main/java/com/ProyectoFinalDWP/ProyectoFinalDwpApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinalDWP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalDwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalDwpApplication.class, args);
+	}
+
+}
